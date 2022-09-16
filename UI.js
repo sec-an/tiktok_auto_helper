a347b9ca3b6ab221fbfd765adf8e0669f3d02f80
sec-an <sec_an@foxmail.com>
2022-09-16 19:35:33 +0800

    Add files via upload

---
+++ UI.js
@@ -0,0 +1,271 @@
+"ui";
+
+importClass(java.net.HttpURLConnection);
+importClass(java.net.URL);
+importClass(java.io.File);
+importClass(java.io.FileOutputStream);
+importClass(android.graphics.Color);
+
+var color = "#FF4FB3FF";
+
+ui.statusBarColor("#FF4FB3FF")
+
+ui.layout(
+    <drawer id="drawer">
+        <vertical>
+            <appbar>
+                <toolbar id="toolbar" bg="#ff4fb3ff" title="抖音助手"/>
+                <tabs id="tabs" bg="#ff4fb3ff"/>
+            </appbar>
+            <viewpager id="viewpager">
+                <frame>
+                    <vertical>
+                        <vertical gravity="center" layout_weight="1">
+                            <card w="*" h="70" margin="10 5" cardCornerRadius="2dp" cardElevation="1dp" foreground="?selectableItemBackground">
+                                <horizontal gravity="center_vertical">
+                                    <vertical padding="10 8" h="auto" w="0" layout_weight="1">
+                                        <text text="悬浮窗权限" textColor="#222222" textSize="16sp" maxLines="1" />
+                                        <text text="请确保开启" textColor="#999999" textSize="14sp" maxLines="1" />
+                                    </vertical>
+                                    <checkbox id="consoleshow" marginLeft="4" marginRight="6" checked="{{floaty.checkPermission()}}" />
+                                </horizontal>
+                            </card>
+                            <card w="*" h="70" margin="10 5" cardCornerRadius="2dp" cardElevation="1dp" foreground="?selectableItemBackground">
+                                <horizontal gravity="center_vertical">
+                                    <vertical padding="10 8" h="auto" w="0" layout_weight="1">
+                                        <text text="音量上键可以停止所有脚本运行" textColor="#222222" textSize="16sp" maxLines="1" />
+                                    </vertical>
+                                </horizontal>
+                            </card>
+                        </vertical>
+                        <button h="60" layout_gravity="center" id="log" textSize="18sp" text="查看日志" />
+                        <button h="60" layout_gravity="center" id="update" textSize="18sp" />
+                        <button id="start" text="开 始 任 务" textSize="25sp" color="#ffffff" bg="#FF4FB3FF" foreground="?selectableItemBackground"/>
+                    </vertical>
+                </frame>
+                <ScrollView>
+                    <frame>
+                        <vertical id="tiktok" gravity="center">
+                            <horizontal gravity="center_vertical" padding="5 5" >
+                                <View bg="#00BFFF" h="*" w="10"  ></View>
+                                <vertical padding="10 8" h="auto" w="0" layout_weight="1">
+                                    <text w="auto" textColor="#222222" textSize="15sp" text="点赞次数" />
+                                    <text w="auto" textColor="#999999" textSize="12sp" text="连赞" />
+                                    <text w="auto" textColor="#999999" textSize="12sp" text="先在抖音打开直播间" />
+                                </vertical> 
+                                <input id="tiktok_like_count" marginLeft="4" marginRight="6" text="1000"  hint="秒"  textSize="13sp"  inputType="number" />
+                            </horizontal>
+                            <horizontal>
+                                <button style="Widget.AppCompat.Button.Colored" id="tiktok_save" text="保存配置" padding="12dp" w="*" />
+                            </horizontal>
+                            <horizontal>
+                                <button style="Widget.AppCompat.Button.Colored" id="tiktok_reset" text="恢复默认" padding="12dp" w="*" />
+                            </horizontal>
+                        </vertical>
+                    </frame>
+                </ScrollView>
+            </viewpager>
+        </vertical>
+    </drawer>
+);
+
+ui.update.visibility = 8;
+
+http.__okhttp__.setTimeout(10000);
+
+
+var GLOBAL_CONFIG = storages.create("GLOBAL_CONFIG");
+var TIKTOK_CONFIG = storages.create("TIKTOK_CONFIG");
+var execution = "";
+var thread = null;
+Initialize();
+
+// 版本更新检查
+var apkurl = "https://ghproxy.com/https://github.com/sec-an/tiktok_auto_helper/releases/download/v1.0.0/v1.0.0.apk";
+var latest_version = "1.0.0";
+if (GLOBAL_CONFIG.get("NO_UPDATE", 0) && (app.versionName != latest_version)) {
+    ui.update.visibility = 0;
+    ui.update.setText("点击更新至最新版v" + latest_version);
+} else if (app.versionName != latest_version) {
+    checkversion();
+}
+
+
+// 创建选项菜单(右上角)
+ui.emitter.on("create_options_menu", menu=>{
+    menu.add("日志");
+    menu.add("关于");
+    menu.add("Github");
+});
+
+// 监听选项菜单点击
+ui.emitter.on("options_item_selected", (e, item)=>{
+    switch(item.getTitle()){
+        case "日志":
+            app.startActivity("console");
+            break;
+        case "关于":
+            alert("关于", "抖音助手 v"+latest_version);
+            break;
+        case "Github":
+            app.openUrl("https://github.com/sec-an/tiktok_auto_helper");
+            break;
+    }
+    e.consumed = true;
+});
+activity.setSupportActionBar(ui.toolbar);
+
+//设置滑动页面的标题
+ui.viewpager.setTitles(["首页", "配置"]);
+//让滑动页面和标签栏联动
+ui.tabs.setupWithViewPager(ui.viewpager);
+
+// 用户勾选无障碍服务的选项时，跳转到页面让用户去开启 
+// android.permission.SYSTEM_ALERT_WINDOW
+// ui.autoService.on("check", function (checked) {
+//     if (checked && auto.service == null) {
+//         app.startActivity({
+//             action: "android.settings.ACCESSIBILITY_SETTINGS"
+//         });
+//     }
+//     if (!checked && auto.service != null) {
+//         auto.service.disableSelf();
+//     }
+// });
+
+// 悬浮窗权限
+ui.consoleshow.on("check", function (checked) {
+    if (checked && !floaty.checkPermission()) {
+        app.startActivity({
+            packageName: "com.android.settings",
+            className: "com.android.settings.Settings$AppDrawOverlaySettingsActivity",
+            data: "package:" + context.getPackageName(),
+        });
+    }
+});
+ 
+// 当用户回到本界面时，resume事件会被触发
+ui.emitter.on("resume", function () {
+    // 此时根据无障碍服务的开启情况，同步开关的状态
+    // ui.autoService.checked = auto.service != null;
+    ui.consoleshow.checked = floaty.checkPermission();
+});
+
+// 保存天天向上pro脚本设置
+ui.tiktok_save.click(function () {
+    TIKTOK_CONFIG.put("tiktok_like_count", ui.tiktok_like_count.getText()+"");
+    toastLog("配置保存成功！");
+});
+
+// 重置天天向上pro脚本设置
+ui.tiktok_reset.click(function () {
+    TIKTOK_CONFIG.put("tiktok_like_count", "100");
+    ui.ttxs_pro_watchdog.setText(TIKTOK_CONFIG.get("tiktok_like_count"));
+    toastLog("配置恢复默认！");
+});
+
+// 打开日志
+ui.log.click(function () {
+    app.startActivity("console");
+});
+
+// 读取脚本设置
+function Initialize() {
+    ui.tiktok_like_count.setText(TIKTOK_CONFIG.get("tiktok_like_count", "100"));
+}
+
+// APP更新检测
+ui.update.click(function () {
+    if (app.versionName != latest_version) {
+        GLOBAL_CONFIG.put("NO_UPDATE", 0);
+        checkversion();
+    } else {
+        toast("当前已经是最新版本！");
+    }
+});
+
+// 下载并运行所选脚本
+ui.start.click(function () {
+    threads.shutDownAll();
+    if (thread != null && thread.isAlive()) {
+        alert("注意", "脚本正在运行，请结束之前进程");
+        return;
+    }
+    threads.start(function () {
+        let url = 'https://ghproxy.com/https://raw.githubusercontent.com/sec-an/tiktok_auto_helper/main/live_like.js';
+        execution = engines.execScript("抖音助手", http.get(url).body.string());
+    });
+});
+
+// APP更新提示
+function checkversion() {
+    var releaseNotes = "版本 v" + latest_version + "\n" +
+        "更新日志:\n" +
+        "* 1.测试"
+    dialogs.build({
+            title: "发现新版本",
+            content: releaseNotes,
+            positive: "立即下载",
+            negative: "取消",
+            neutral: "浏览器下载",
+            checkBoxPrompt: "不再提示",
+            cancelable: false
+        })
+        .on("positive", () => {
+            download(apkurl);
+        })
+        .on("neutral", () => {
+            app.openUrl(apkurl);
+        })
+        .on("check", (checked) => {
+            GLOBAL_CONFIG.put("NO_UPDATE", 1);
+        }).show();
+}
+
+// 打开下载进度面板
+function download(url) {
+    downloadDialog = dialogs.build({
+        title: "正在下载...",
+        progress: {
+            max: 100,
+            showMinMax: true
+        },
+        autoDismiss: false,
+        cancelable: false
+    }).show();
+    startDownload(url);
+}
+
+// 下载apk的主方法体
+function startDownload(url) {
+    threads.start(function () {
+        var path = files.cwd() + "/new.apk";
+        let apkFile = new File(path);
+        var conn = new URL(url).openConnection();
+        conn.connect();
+        let is = conn.getInputStream();
+        let length = conn.getContentLength();
+        let fos = new FileOutputStream(apkFile);
+        let count = 0;
+        let buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+        while (true) {
+            var p = ((count / length) * 100);
+            let numread = is.read(buffer);
+            count += numread;
+            // 下载完成
+            if (numread < 0) {
+                toast("下载完成");
+                downloadDialog.dismiss();
+                downloadDialog = null;
+                break;
+            }
+            // 更新进度条
+            downloadDialog.setProgress(p);
+            fos.write(buffer, 0, numread);
+        }
+        fos.close();
+        is.close();
+        //自动打开进行安装
+        app.viewFile(path);
+    })
+}
---
+++ live_like.js
@@ -0,0 +1,16 @@
+// 读取自定义配置
+var TTXS_PRO_CONFIG = storages.create("TIKTOK_CONFIG");
+var like_count = TIKTOK_CONFIG.get("like_count", "100");
+
+like_count = parseInt(like_count);
+
+setScreenMetrics(1080, 1920);
+
+app.launch("com.ss.android.ugc.aweme");
+
+waitForPackage("com.ss.android.ugc.aweme");
+
+for (var i = 0; i < like_count; i++) {
+    press(random(400, 700), random(700, 800), 1);
+    sleep(random(100, 200));
+}
